52ee4b04444554555b42f0fdba2d41a26155ee35
Larisa Zakharova <zakharoval78@gmail.com>
2024-09-29 00:05:32 +0300

    Update Main.java

--- src/main/java/larisa/zakharova/Main.java
+++ src/main/java/larisa/zakharova/Main.java
@@ -1,9 +1,11 @@
 package larisa.zakharova;
 
 public class Main {
+    private static String msg = "Hello qa.guru!!!!"
+    
     public static void main(String[] args) {
 
-        System.out.printf("Hello qa.guru!!!!");
+        System.out.printf(msg);
 
         for (int i = 1; i <= 5; i++) {
 
