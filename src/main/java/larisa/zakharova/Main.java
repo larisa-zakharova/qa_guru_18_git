f56bd20375f749560b710e8a0b2a6a352c2ab81b
Larisa Zakharova <zakharoval78@gmail.com>
2024-09-29 00:09:43 +0300

    add getMessage() method

--- src/main/java/larisa/zakharova/Main.java
+++ src/main/java/larisa/zakharova/Main.java
@@ -3,11 +3,15 @@ package larisa.zakharova;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello qa.guru");
+        System.out.printf(getMessage());
 
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
         }
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru";
     }
 }
