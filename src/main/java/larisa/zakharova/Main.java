00d52e7efcd466f31d06eb3c2c8f5c2b63b35910
Larisa Zakharova <zakharoval78@gmail.com>
2024-09-29 10:19:03 +0300

    delete cycle for

--- src/main/java/larisa/zakharova/Main.java
+++ src/main/java/larisa/zakharova/Main.java
@@ -1,16 +1,11 @@
 package larisa.zakharova;
 
 public class Main {
-    private static String msg = "Hello qa.guru!!!!";
+    private static String msg = "Hello qa.guru!";
 
     public static void main(String[] args) {
 
         System.out.printf(getMessage());
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
     }
 
     private static String getMessage() {
