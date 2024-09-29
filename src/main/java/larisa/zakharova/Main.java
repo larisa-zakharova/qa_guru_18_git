26dee5bf9649931a7483ce12e241623fb1324521
Larisa Zakharova <zakharoval78@gmail.com>
2024-09-29 10:40:28 +0300

    Update Main.java
    change hello message

--- src/main/java/larisa/zakharova/Main.java
+++ src/main/java/larisa/zakharova/Main.java
@@ -1,13 +1,10 @@
 package larisa.zakharova;
 
 public class Main {
-    private static String msg = "Hello world and qa.guru!!!!!!!";
-
+    private static String msg = "Hello qa.guru!";
     public static void main(String[] args) {
-
         System.out.printf(getMessage());
     }
-
     private static String getMessage() {
         return msg;
     }
